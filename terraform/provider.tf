5b80c0d1089e3b7715c127ce9ec6a08fddeb5fe8
KiraBush <95545736+KiraBush@users.noreply.github.com>
2021-12-05 00:59:00 +0500

    Delete terraform directory

--- terraform/provider.tf
+++
@@ -1,8 +0,0 @@
-provider "schemaregistry" {
-  uri = var.schema_registry_url
-}
-
-provider "kafka" {
-  bootstrap_servers = var.kafka_bootstrap_servers
-  tls_enabled = false
-}
--- terraform/schemas.tf
+++
@@ -1,9 +0,0 @@
-resource "schemaregistry_subject" "source" {
-  subject = "source-value"
-  schema = file("schemas/source.avro.json")
-}
-
-resource "schemaregistry_subject" "response" {
-  subject = "response-value"
-  schema = file("schemas/response.avro.json")
-}
